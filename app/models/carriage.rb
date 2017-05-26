c0d9ca9d0258bd73ffafb5211122dd115c30dec8
Mark Frost <cheerful.mf@gmail.com>
2017-05-26 11:47:26 +0500

    Add scopes for carriage
    Count by kind
    Count seats by kind

--- app/models/carriage.rb
+++ app/models/carriage.rb
@@ -6,4 +6,8 @@ class Carriage < ApplicationRecord
   validates :top_seats, :low_seats, inclusion: 0..50
 
   validates :kind, inclusion: { in: kinds }
+
+  scope :count_by_kind, ->(kind) { where(kind: kind).count }
+
+  scope :count_seats_by_kind, ->(kind, position) { where(kind: kind).map(&position).reduce(:+) }
 end
