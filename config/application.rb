5d86cb201cee4b02ccdce529f6accd89575af347
Mark Frost <cheerful.mf@gmail.com>
2017-07-01 16:24:05 +0500

    fixes

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module RailwayRor
 
     config.i18n.default_locale = :ru
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
